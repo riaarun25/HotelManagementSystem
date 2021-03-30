692c878541391235ca8d1bb71668c868ac61374e
Ria Arun <81374415+riaarun25@users.noreply.github.com>
2021-03-30 14:32:53 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+    include('index.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Imperial</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+        <a href='home.php'><h1>Hotel Management System</h1></a>
+            <br>
+            <table width="100%" border="50" cellspacing="0">
+                <tr>
+                    <td><a href="customer login.php"><h2>Customer Login</h2></a></td>
+                   <td><a href="employee login.php"><h2>Employee Login</h2></a></td>
+                </tr>
+            </table>
+        </div>
+    </header>
+</body>
+<div class="footer">
+    <h4>Copyright 2020 Hotel Imperial | All rights reserved.</h4>
+</div>
+</html>
